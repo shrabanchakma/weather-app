28bffe75904b35274993185f69b2afd6e69131d4
Shraban Chakma <shrabanchakma328@gmail.com>
2024-02-03 21:58:44 +0600

    added feature to show emoji according to the weather

--- index.js
+++ index.js
@@ -65,7 +65,26 @@ function displayWeatherData(data) {
   card.appendChild(weatherEmoji);
 }
 
-function getWeatherEmoji(weatherId) {}
+function getWeatherEmoji(weatherId) {
+  switch (true) {
+    case weatherId >= 200 && weatherId < 300:
+      return "🌧️";
+    case weatherId >= 300 && weatherId < 400:
+      return "🌧️";
+    case weatherId >= 500 && weatherId < 600:
+      return "🌧️";
+    case weatherId >= 600 && weatherId < 700:
+      return "❄️";
+    case weatherId >= 700 && weatherId < 800:
+      return "🌫️";
+    case weatherId === 800:
+      return "🌞";
+    case weatherId >= 801 && weatherId < 810:
+      return "☁️";
+    default:
+      return "?";
+  }
+}
 
 function displayError(message) {
   const errorElement = document.createElement("p");
